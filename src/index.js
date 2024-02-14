102f6f30df4da618b9759be1ba8e7e838944965f
NaraVictor <naravictor@live.com>
2024-02-14 14:41:49 +0000

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import posthog from 'posthog-js'
-
-// Logging and Analytics like LogRocket (maybe also try this)
-posthog.init( 'phc_8bPTAAcuqV2RHUsnO9PSrWEUfkphA2D1b1RXxZlHw6t', { api_host: 'https://app.posthog.com' } )
-posthog.capture( 'bokxPOS start', { property: 'start here' } )
 
 const root = ReactDOM.createRoot( document.getElementById( "root" ) );
 root.render(
