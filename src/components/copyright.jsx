31fbcca829534394d7f0bba0866166462926f68b
NaraVictor <naravictor@live.com>
2024-02-14 14:56:33 +0000

    updated copyright

--- src/components/copyright.jsx
+++ src/components/copyright.jsx
@@ -1,7 +1,7 @@
 const Copyright = ( props ) => {
     return (
         <>
-            Copyright &copy; { new Date().getFullYear() } Waffle LLC <span className="mx-2"> - </span>BokxPOS 1.0.0
+            Copyright &copy; { new Date().getFullYear() } Goil
         </>
     );
 }
